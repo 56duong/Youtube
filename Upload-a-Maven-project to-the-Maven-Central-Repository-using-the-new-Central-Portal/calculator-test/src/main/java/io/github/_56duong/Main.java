c294edacb830ca794deb6d8904c68b60e3c9aacb
56duong <nguyenduong07122003@gmail.com>
2024-07-06 17:54:47 +0700

    Upload-a-Maven-project to-the-Maven-Central-Repository-using-the-new-Central-Portal

---
+++ Upload-a-Maven-project to-the-Maven-Central-Repository-using-the-new-Central-Portal/calculator-test/src/main/java/io/github/_56duong/Main.java
@@ -0,0 +1,9 @@
+package io.github._56duong;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(3, 9);
+        System.out.println(sum);
+    }
+}
